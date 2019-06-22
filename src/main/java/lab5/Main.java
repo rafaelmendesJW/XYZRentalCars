97402d7874b631b1e62f9c8f4e706fc5682e6fb4
Mauro Pinheiro <mauro.pinheiro@acad.ifma.edu.br>
2019-06-22 20:30:11 -0300

    Criado o metodo main para executar o codigo Falta colocar o nome da persistence unit na hora de criar o Entity manager

---
+++ src/main/java/lab5/Main.java
@@ -0,0 +1,12 @@
+package lab5;
+
+import javax.persistence.Persistence;
+
+public class Main{
+    public static void main(String[] args) {
+        //Coloca entre as aspas o nome da persistence unit
+        //Se encontra no arquivo xml dentro da pasta meta-inf
+        //Uma das primeiras linhas
+        Persistence.createEntityManagerFactory("")
+    }
+}
